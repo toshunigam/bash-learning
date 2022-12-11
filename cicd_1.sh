059f9492bdf30b9d4b5a435e2ee47314d37aca3d
toshunigam <toshuchandra@gmail.com>
2022-12-11 21:28:11 +0530

    working into ci cd example

---
+++ cicd_1.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+PROJECT_DIR='E:\my-projects\nodelargefileupload'
+PROJECT_BACKUP='E:\my-projects\backup\backup_'
+
+cp -r $PROJECT_DIR $PROJECT_BACKUP`date +'%Y%m%d_%H%M%S'`
+echo 'Successfully backeuped files'
+cd $PROJECT_DIR
+echo 'removing node_modules and package-lock files'
+rm -rf node_modules
+rm -r package-lock.json
+echo 'installing project...'
+npm install
+echo 'package installed'
