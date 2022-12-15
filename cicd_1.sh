644b577bddcbb1f20e9452f42ee2d345beb51dd7
toshunigam <toshuchandra@gmail.com>
2022-12-15 22:00:47 +0530

    successfully tested.

--- cicd_1.sh
+++ cicd_1.sh
@@ -2,8 +2,9 @@
 PROJECT_DIR='E:\my-projects\nodelargefileupload'
 PROJECT_BACKUP='E:\my-projects\backup\backup_'
 
+echo 'Backup of file is started...'
 cp -r $PROJECT_DIR $PROJECT_BACKUP`date +'%Y%m%d_%H%M%S'`
-echo 'Successfully backeuped files'
+echo 'Successfully backeuped files.'
 cd $PROJECT_DIR
 echo 'removing node_modules and package-lock files'
 rm -rf node_modules
