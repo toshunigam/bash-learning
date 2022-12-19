3e373985f3eb3713503fcf3f5410503545b66c9f
toshunigam <toshuchandra@gmail.com>
2022-12-19 22:31:02 +0530

    colored text in bash

---
+++ text-decoration.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+REDBOLD='\033[0;31;1m'
+GREENBOLD='\033[0;32;1m'
+BOLD='\033[1m'
+NORMAL='\033[0m'
+
+echo -e "${BOLD} I am bold"
+echo -e "${NORMAL} I am noraml "
+echo -e "${GREENBOLD} I am green and bold "
+echo -e "${REDBOLD} I am red and bold "
